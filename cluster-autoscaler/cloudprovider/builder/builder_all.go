9654f2b87d45790d842e8b1c75f963ff664408a4
Van Thong Nguyen <nvthongswansea@gmail.com>
2023-04-23 17:59:33 +0200

    add gridscale autoscaler provider

--- cluster-autoscaler/cloudprovider/builder/builder_all.go
+++ cluster-autoscaler/cloudprovider/builder/builder_all.go
@@ -35,6 +35,7 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/exoscale"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/externalgrpc"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gridscale"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/hetzner"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/huaweicloud"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/ionoscloud"
@@ -145,6 +146,8 @@ func buildCloudProvider(opts config.AutoscalingOptions, do cloudprovider.NodeGro
 		return rancher.BuildRancher(opts, do, rl)
 	case cloudprovider.VolcengineProviderName:
 		return volcengine.BuildVolcengine(opts, do, rl)
+	case cloudprovider.GridscaleProviderName:
+		return gridscale.BuildGridscale(opts, do, rl)
 	}
 	return nil
 }
